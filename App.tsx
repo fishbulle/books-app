3ca28725071c674d6b40771aae4cfb7a54ee52e3
Angelina <linaemalmros@gmail.com>
2023-05-16 11:07:10 +0200

    flyttade navigatorn till en egen komponent

--- App.tsx
+++ App.tsx
@@ -1,63 +1,7 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import HomeScreen from './screens/HomeScreen';
-import ProfileScreen from './screens/ProfileScreen';
-import Ionicons from '@expo/vector-icons/Ionicons';
-import LibraryScreen from './screens/LibraryScreen';
-
-const Tab = createBottomTabNavigator()
+import TabNav from './components/TabNav';
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Tab.Navigator
-        screenOptions={() => ({
-          tabBarActiveTintColor: '#800000',
-          tabBarInactiveTintColor: 'gray',
-          tabBarStyle: { paddingTop: 2, height: 60 },
-          tabBarLabelStyle: { paddingBottom: 8 }
-        })}
-      >
-        <Tab.Screen
-          options={{
-            tabBarIcon: ({ focused }) => {
-              return <Ionicons
-                name={focused ? 'home' : 'home-outline'}
-                color={focused ? '#800000' : 'black'}
-                size={24} />
-            },
-            headerShown: false
-          }}
-          name="Home"
-          component={HomeScreen}
-        />
-        <Tab.Screen
-          options={{
-            tabBarIcon: ({ focused }) => {
-              return <Ionicons
-                name={focused ? 'book' : 'book-outline'}
-                color={focused ? '#800000' : 'black'}
-                size={24} />
-            },
-            headerShown: false
-          }}
-          name="Library"
-          component={LibraryScreen}
-        />
-        <Tab.Screen
-          options={{
-            tabBarIcon: ({ focused }) => {
-              return <Ionicons
-                name={focused ? 'person' : 'person-outline'}
-                color={focused ? '#800000' : 'black'}
-                size={24} />
-            },
-            headerShown: false
-          }}
-          name="Profile"
-          component={ProfileScreen}
-        />
-      </Tab.Navigator>
-    </NavigationContainer>
+    <TabNav />
   )
 }
