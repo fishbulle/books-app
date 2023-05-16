8607bd472acb22f1dd77021a689850c836fcd9f8
Angelina <linaemalmros@gmail.com>
2023-05-16 10:47:49 +0200

    bottom tab navigator klar

--- App.tsx
+++ App.tsx
@@ -1,10 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
-import { View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createAppContainer } from "react-navigation";
+import HomeScreen from './screens/HomeScreen';
+import ProfileScreen from './screens/ProfileScreen';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import MyBooks from './screens/MyBooks';
+
+const Tab = createBottomTabNavigator()
 
 export default function App() {
   return (
-    <View>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={() => ({
+          tabBarActiveTintColor: 'black',
+          tabBarInactiveTintColor: 'gray',
+          tabBarStyle: { paddingTop: 2, height: 60 },
+          tabBarLabelStyle: { paddingBottom: 8 }
+        })}
+      >
+        <Tab.Screen
+          options={{
+            tabBarIcon: ({ focused }) => {
+              return <Ionicons
+                name={focused ? 'home' : 'home-outline'}
+                color='black'
+                size={24} />
+            },
+          }}
+          name="Home"
+          component={HomeScreen}
+        />
+        <Tab.Screen
+          options={{
+            tabBarIcon: ({ focused }) => {
+              return <Ionicons
+                name={focused ? 'book' : 'book-outline'}
+                color='black'
+                size={24} />
+            },
+          }}
+          name="Library"
+          component={MyBooks}
+        />
+        <Tab.Screen
+          options={{
+            tabBarIcon: ({ focused }) => {
+              return <Ionicons
+                name={focused ? 'person' : 'person-outline'}
+                color='black'
+                size={24} />
+            },
+          }}
+          name="Profile"
+          component={ProfileScreen}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
+
---
+++ screens/MyBooks.tsx
@@ -0,0 +1,14 @@
+import { View } from "react-native"
+
+const MyBooks = () => {
+
+    // komponenter:
+    // böcker med info
+    // lista på mest lästa genres
+
+    return (
+        <View></View>
+    )
+}
+
+export default MyBooks
