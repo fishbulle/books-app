ed724dc05dbe129fa82fe705dfd4023e79aeda6a
Angelina <linaemalmros@gmail.com>
2023-05-16 09:46:06 +0200

    börjat planera och strukturera upp projektet

---
+++ screens/HomeScreen.tsx
@@ -0,0 +1,15 @@
+import { View } from "react-native"
+
+const HomeScreen = () => {
+
+    // komponenter:
+    // böcker med info
+    // knapp (MyButton) för att visa mer info i en modal
+    // lista på genres
+
+    return (
+        <View></View>
+    )
+}
+
+export default HomeScreen
---
+++ screens/ProfileScreen.tsx
@@ -0,0 +1,15 @@
+import { View } from "react-native";
+
+const ProfileScreen = () => {
+
+    // komponenter:
+    // profilbild
+    // namn (text)
+    // input favoritböcker
+
+    return (
+        <View></View>
+    )   
+}
+
+export default ProfileScreen
